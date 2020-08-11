5e5b645286181cf3813076d3a22eb7a3dfcce4e6
Godstime Israel <godstimeisrael66@gmail.com>
2020-08-11 17:10:40 +0000

    Implemented express app and started implementation on the Story resource specifically the createStory method(POST)

---
+++ web-api/src/index.js
@@ -0,0 +1,23 @@
+// const server = require("./infra/server");
+
+// const main = async () => {
+//   // Run Server
+//   await server()
+// };
+
+// main();
+
+
+/***************************/ 
+// Weapon
+class Weapon{
+  name: null,
+  bullet: 
+}
+
+// Character for a game
+class Character {
+  name = null;
+  life= 0
+  weapon: 
+}
